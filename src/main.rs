5d17c99807d91541e8d0a3bf0d602ca6c4b561a5
Valerian Saliou <valerian@valeriansaliou.name>
2025-01-15 19:50:04 -0300

    Add base Rust structure

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// Bandurria
+//
+// Lightweight comment system for static websites
+// Copyright: 2025, Valerian Saliou <valerian@valeriansaliou.name>
+// License: Mozilla Public License v2.0 (MPL v2.0)
+
+#[macro_use]
+extern crate log;
+
+fn main() {
+    info!("starting up");
+
+    // TODO
+
+    error!("could not start");
+}
