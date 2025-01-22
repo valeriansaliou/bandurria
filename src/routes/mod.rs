2916df6c5caa28684fa8b87ed0b07057375b31f9
Valerian Saliou <valerian@valeriansaliou.name>
2025-01-22 09:02:48 -0300

    Clear

--- src/routes/mod.rs
+++
@@ -1,8 +0,0 @@
-// Bandurria
-//
-// Lightweight comment system for static websites
-// Copyright: 2025, Valerian Saliou <valerian@valeriansaliou.name>
-// License: Mozilla Public License v2.0 (MPL v2.0)
-
-pub mod api;
-pub mod page;
